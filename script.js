b7d44d9c6140d619c71b0a7721fa1ea24de80b74
Abid ali <khanali3229>
2024-09-22 15:03:31 +0500

    firstly i selected elements

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strick";
+
+//selecting elements
+
+const score0El = document.querySelector("#score--0");
+
+const score1El = document.querySelector("#score--1 ");
+const diceHi = document.querySelector(".dice");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceHi.classList.add("hidden");
