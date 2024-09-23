3b038db947cdc57618e126f8b14ea172dd5b2b7d
Abid ali <khanali3229>
2024-09-23 11:35:23 +0500

    check the players score is equal to 20  so the will be win and the back-ground color is black

--- script.js
+++ script.js
@@ -61,4 +61,17 @@ btnHold.addEventListener("click", function () {
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. Check if player's score is >= 100
+  if (scores[activePlayer] >= 20) {
+    // Finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add(`player--winner`);
+
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
+  } else {
+    // Switch to the next player
+    switchPlayer();
+  }
 });
