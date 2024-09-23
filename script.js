a8b3c1d6f4d86af33faea2e2ec5a048106ac7f82
Abid ali <khanali3229>
2024-09-23 09:58:58 +0500

    leave comments to read easily and also do changes in code

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ const btnNew = document.querySelector(".diceNew");
 const btnRoll = document.querySelector(".diceRoll");
 const btnHold = document.querySelector(".diceHold");
 
+//starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+//Rolling dice funtionality
+btnRoll.addEventListener("click", function () {
+  //1. Generating a rendom number
+  const dice = Math.trunc(Math.random() * 6) + 1;
 
-const dice = Math.trunc(Math.random() * 50) + 4;
-
-btnRoll.addEventListener("click", function () {});
+  //2.Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice${dice}.png`;
+  //3.Check for rolled 1: if true,switch to next player
+});
