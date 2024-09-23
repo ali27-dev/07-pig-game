3e7883f784728ad16d9f4889ae5ae54d72257612
Abid ali <khanali3229>
2024-09-23 10:49:40 +0500

    if dice is not eqaul to 1 then the score is added else if dice is  equal to 1 then switch the player and process will repeat

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 //selecting elements
 
 const score0El = document.querySelector("#score--0");
-
 const score1El = document.querySelector("#score--1 ");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".palyer--0");
+const player1El = document.querySelector(".palyer--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--New");
 const btnRoll = document.querySelector(".btn--roll");
@@ -18,6 +20,7 @@ score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
 let currentScore = 0;
+let activePlayer = 0;
 //Rolling dice funtionality
 btnRoll.addEventListener("click", function () {
   //1. Generating a rendom number
@@ -26,13 +29,19 @@ btnRoll.addEventListener("click", function () {
   //2.Display dice
   diceEl.classList.remove("hidden");
   diceEl.src = `images/dice-${dice}.png`;
-  //3.Check for rolled 1: if true,switch to next player
+  //3.Check for rolled 1:
 
   if (dice !== 1) {
     //Add dice to current score
+
     currentScore = currentScore + dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
+// if true,switch to next player
