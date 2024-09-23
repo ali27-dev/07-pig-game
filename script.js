64bba1d4f264fd2d415e2c645639387b3dc18eec
Abid ali <khanali3229>
2024-09-23 09:51:10 +0500

    i seleted another elements and then i give functiolaty to dice button

--- script.js
+++ script.js
@@ -5,8 +5,15 @@
 const score0El = document.querySelector("#score--0");
 
 const score1El = document.querySelector("#score--1 ");
-const diceHi = document.querySelector(".dice");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".diceNew");
+const btnRoll = document.querySelector(".diceRoll");
+const btnHold = document.querySelector(".diceHold");
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-diceHi.classList.add("hidden");
+diceEl.classList.add("hidden");
+
+const dice = Math.trunc(Math.random() * 50) + 4;
+
+btnRoll.addEventListener("click", function () {});
