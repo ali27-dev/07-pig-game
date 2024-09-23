36c11836334ff4179d59b9a94272b92f669cde6b
Abid ali <khanali3229>
2024-09-23 10:17:49 +0500

    to roll dice i added if else statment to add current score and dice and also select elemnts

--- script.js
+++ script.js
@@ -5,15 +5,19 @@
 const score0El = document.querySelector("#score--0");
 
 const score1El = document.querySelector("#score--1 ");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".diceNew");
-const btnRoll = document.querySelector(".diceRoll");
-const btnHold = document.querySelector(".diceHold");
+const btnNew = document.querySelector(".btn--New");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--Hold");
 
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+
+let currentScore = 0;
 //Rolling dice funtionality
 btnRoll.addEventListener("click", function () {
   //1. Generating a rendom number
@@ -21,6 +25,14 @@ btnRoll.addEventListener("click", function () {
 
   //2.Display dice
   diceEl.classList.remove("hidden");
-  diceEl.src = `dice${dice}.png`;
+  diceEl.src = `images/dice-${dice}.png`;
   //3.Check for rolled 1: if true,switch to next player
+
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore = currentScore + dice;
+    current0El.textContent = currentScore;
+  } else {
+    //Switch to next player
+  }
 });
